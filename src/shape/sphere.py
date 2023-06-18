8796b06b086b55e708819c29cc57227720b15f24
philehteo <m.theophile.arnould@gmail.com>
2023-06-18 21:50:26 +0200

    sphere delete

--- src/shape/sphere.py
+++
@@ -1,8 +0,0 @@
-from OpenGL.GL import *
-import numpy as np
-from shape.geometricShape import GeometricShape
-
-class Sphere(GeometricShape):
-
-    def __init__(self, position, eulers):
-        super().__init__(position, eulers)
